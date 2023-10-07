d0d47bf7629bb3e76e3425313a360606c68e1a76
Naresh2424 <nareshavula4422@gmail.com>
2023-10-07 12:34:39 +0530

    add some code

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const add = require('./add');
 console.log("Hello from index.js");
+
+const sum = add(2,7);
+const sum2 = add(2,10);
+
+console.log(`sum of two numbers is`,sum);
+console.log(`sum of two numbers is`,sum2);
